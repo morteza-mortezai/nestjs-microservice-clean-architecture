916e05340a7378dc8e1543ddb548a3e5e88f4269
morteza-mortezai <mrtz.mrtzi.1989@gmail.com>
2023-04-22 23:20:16 +0430

    update

--- libs/common/src/filter/exception.filter.ts
+++ libs/common/src/filter/exception.filter.ts
@@ -1,22 +1,31 @@
-import { ExceptionFilter, Catch, ArgumentsHost, HttpException } from '@nestjs/common';
+import { ExceptionFilter, Catch, ArgumentsHost, HttpException, HttpStatus } from '@nestjs/common';
 import { Request, Response } from 'express';
-
+interface IError {
+  message: string;
+}
 @Catch(HttpException)
 export class HttpExceptionFilter implements ExceptionFilter {
-  catch(exception: HttpException, host: ArgumentsHost) {
+  catch(exception: any, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const request = ctx.getRequest<Request>();
-    const status = exception.getStatus();
-    const message = exception.getResponse();
+    const status = exception instanceof HttpException ? exception.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+    // const message = exception.getResponse();
+    const message =
+      exception instanceof HttpException
+        ? (exception.getResponse() as IError)
+        : { message: (exception as IError).message };
 
+    const responseData = {
+      ...{
+        statusCode: status,
+        timestamp: new Date().toISOString(),
+        path: request.url
+      },
+      ...message,
+    }
     response
       .status(status)
-      .json({
-        statusCode: status,
-        message,
-        timestamp: new Date().toISOString(),
-        path: request.url,
-      });
+      .json(responseData);
   }
 }
