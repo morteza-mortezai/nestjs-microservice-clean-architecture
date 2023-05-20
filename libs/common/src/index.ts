098397db86e2154e875c8f94357cea5b35e322f2
morteza-mortezai <mrtz.mrtzi.1989@gmail.com>
2023-05-20 23:34:36 +0430

    some movement

--- libs/common/src/index.ts
+++ libs/common/src/index.ts
@@ -1,5 +1,3 @@
-export * from './rmq/rmq.module';
-export * from './rmq/rmq.service';
 export * from './disk-storage/disk-store.module';
 export * from './disk-storage/disk-store.service';
 export * from './generic-repository/generic.repository'
